41c2b6fc48ab5470da5e778b5e2012dfb50e2784
Shannen <shannen.ye@gmail.com>
2017-11-07 10:44:31 -0500

    Twitter

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.use('/', function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    console.log('Request URL:', req.originalUrl)
+
+    next()
+}, function (req, res, next) {
+    console.log('Request Type:', req.method)
+    next()
+})
